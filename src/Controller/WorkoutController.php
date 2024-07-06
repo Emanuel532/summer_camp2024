f4d3a49efaeb3fb076604cd4a37c34114be5fa18
Emanuel532 <ungureanuemanuelconstantin@yahoo.com>
2024-07-06 22:37:04 +0300

    add workout edit(+ exerciseLogs edit)

--- src/Controller/WorkoutController.php
+++ src/Controller/WorkoutController.php
@@ -93,11 +93,93 @@ class WorkoutController extends AbstractController
     }
 
     #[Route('/workouts/{id}/edit', name: 'workouts_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Workout $workout, EntityManagerInterface $entityManager, ExercitiiRepository $exercitiiRepository,TipRepository $tipRepository, UserRepository $userRepository): Response
+    public function edit(Request $request, ExerciseLogRepository $exerciseLogRepository, Workout $workout, EntityManagerInterface $entityManager, ExercitiiRepository $exercitiiRepository,TipRepository $tipRepository, UserRepository $userRepository): Response
     {
+
+
+        //process the request if there is one
+        $arrayValori = [];
+        foreach ($request->getPayload() as $key => $value) {
+            $arrayValori[$key] = $value;
+        }
+
+        if($arrayValori != []) {
+
+            $logs = $exerciseLogRepository->findAllExerciseLogs($workout->getId());
+            //update the workout!
+            $workout->setNume($arrayValori['nume']);
+            $user = $userRepository->find($arrayValori['user']);
+            $workout->setUser($user);
+            $entityManager->persist($workout);
+            $entityManager->flush();
+            if ($arrayValori['nume'] != null and $arrayValori['user'] != null) { //request validation
+
+                if(isset($arrayValori['exercise_id']))
+                if (count($logs) > count($arrayValori['exercise_id'])) //if we need to delete the exerciselogs
+                {
+                    foreach ($logs as $log) {
+                        $entityManager->remove($log);
+                    }
+
+
+                    $date = DateTime::createFromFormat('Y-m-d', $arrayValori['date']);
+                    $workout->setDate($date);
+
+                    $tip = $tipRepository->find(3);
+
+                    $workout->setTipId($tip);
+                    $entityManager->persist($workout);
+                    $entityManager->flush();
+
+
+                    for ($i = 0; $i < count($arrayValori['exercise_id']); $i++) {
+                        $exerciseLog = new ExerciseLog();
+
+                        $exercitiu = $exercitiiRepository->find($arrayValori['exercise_id'][$i]);
+                        $exerciseLog->setExercise($exercitiu);
+
+                        $exerciseLog->setWorkout($workout);
+                        $exerciseLog->setNrReps($arrayValori['nr_reps'][$i]);
+
+
+                        $exerciseLog->setDuration($arrayValori['duration'][$i]);
+                        $entityManager->persist($exerciseLog);
+                        $entityManager->flush();
+                    }
+                    return $this->redirectToRoute('workouts_show', ['id' => $workout->getId()], Response::HTTP_SEE_OTHER);
+
+                } else {
+                    $i = 0;
+                    for (; $i < count($logs); $i++) {
+                        $logs[$i]->setExercise($exercitiiRepository->find($arrayValori['exercise_id'][$i]));
+                        $logs[$i]->setNrReps($arrayValori['nr_reps'][$i]);
+                        $logs[$i]->setDuration($arrayValori['duration'][$i]);
+                        $entityManager->persist($logs[$i]);
+                    }
+
+                    for (; $i < count($arrayValori['exercise_id']); $i++) {
+
+                        $exerciseLog = new ExerciseLog();
+                        $exerciseLog->setExercise($exercitiiRepository->find($arrayValori['exercise_id'][$i]));
+                        $exerciseLog->setWorkout($workout);
+                        $exerciseLog->setNrReps($arrayValori['nr_reps'][$i]);
+                        $exerciseLog->setDuration($arrayValori['duration'][$i]);
+                        $entityManager->persist($exerciseLog);
+                    }
+                    $entityManager->flush();
+
+                }
+
+            }
+            return $this->redirectToRoute('workouts_show', ['id' => $workout->getId()], Response::HTTP_SEE_OTHER);
+        }
+
+
+
 
         $users = $userRepository->findAll();
         $exercises = $exercitiiRepository->findAll();
+
 
 
         return $this->render('workouts/edit_workouts.html.twig', [
